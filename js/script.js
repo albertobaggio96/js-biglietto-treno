866da563a93e10ddb556aa2702967fe430584c58
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-21 15:03:50 +0100

    enter arabic number only

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-const userKm= parseInt(prompt("Quanti km vuoi percorrere?"));
+let userKm= parseInt(prompt("Quanti km vuoi percorrere?"));
 
-const userAge= parseInt(prompt("Quanti anni hai?"));
+if (isNaN(userKm)){
+  userKm = parseInt(prompt ("Per favore, indicare i km usa solo numeri arabi, Grazie"));
+}
+
+let userAge= parseInt(prompt("Quanti anni hai?"));
+
+if (isNaN(userAge)){
+  userKm = parseInt(prompt ("Per favore, indicare la tua età usa solo numeri arabi, Grazie"));
+}
 
 const euroKm= 0.12;
 
 let ticketCost= userKm * euroKm;
 
-console.log("Prezzo intero")
-console.log(ticketCost)
+console.log("Prezzo intero");
+console.log(ticketCost);
+
 
 if (userAge < 18){
   console.log("Prezzo per minorenni")
@@ -16,8 +25,8 @@ if (userAge < 18){
   console.log("Prezzo per over 65")
   ticketCost= ticketCost * 60 / 100;
 }else{
-  console.log("Ti tieni il prezzo intero")
+  console.log("Ti tieni il prezzo intero");
 }
-console.log(ticketCost)
+console.log(ticketCost);
 
-document.getElementById("output").innerHTML= `Il tuo biglietto costa ${ticketCost}&euro;`
+document.getElementById("output").innerHTML= `Il tuo biglietto costa ${ticketCost}&euro;`;
