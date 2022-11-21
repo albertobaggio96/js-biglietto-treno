89446348d2d1b4515ed21c27a0ae6815d4df8c00
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-21 14:32:22 +0100

    Discount for under 18 and over 65

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-const userKm= parseInt(prompt("Quanti km vuoi percorrere?"))
+const userKm= parseInt(prompt("Quanti km vuoi percorrere?"));
 
-const userAge= parseInt(prompt("Quanti anni hai?"))
+const userAge= parseInt(prompt("Quanti anni hai?"));
 
-const euroKm= 0.12
+const euroKm= 0.12;
 
-let ticketCost= userKm * euroKm
+let ticketCost= userKm * euroKm;
 
+console.log("Prezzo intero")
+console.log(ticketCost)
+
+if (userAge < 18){
+  console.log("Prezzo per minorenni")
+  ticketCost= ticketCost * 80 / 100;
+} else if (userAge > 65){
+  console.log("Prezzo per over 65")
+  ticketCost= ticketCost * 60 / 100;
+}else{
+  console.log("Ti tieni il prezzo intero")
+}
 console.log(ticketCost)
 
 document.getElementById("output").innerHTML= `Il tuo biglietto costa ${ticketCost}&euro;`
