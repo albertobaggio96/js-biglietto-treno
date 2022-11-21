d40319764a2658a927f360d44d3cad9d58ec7606
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-21 16:05:57 +0100

    check if thay use number

--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
 let userKm= parseInt(prompt("Quanti km vuoi percorrere?"));
 
-if (isNaN(userKm)){
-  userKm = parseInt(prompt ("Per favore, per indicare i km usa solo numeri arabi, Grazie"));
-}
-
 let userAge= parseInt(prompt("Quanti anni hai?"));
 
-if (isNaN(userAge)){
-  userKm = parseInt(prompt ("Per favore, per indicare la tua età usa solo numeri arabi, Grazie"));
-}
-
-const euroKm= 0.12;
-
-let ticketCost= userKm * euroKm;
-
-console.log("Prezzo intero");
-console.log(ticketCost);
-
-if (userAge < 18){
-  console.log("Prezzo per minorenni")
-  ticketCost= ticketCost * 80 / 100;
-} else if (userAge > 65){
-  console.log("Prezzo per over 65")
-  ticketCost= ticketCost * 60 / 100;
+if ((isNaN(userKm)) || (isNaN(userAge))){
+document.getElementById("output").innerHTML= `I dati inseriti non sono corretti, per favore ricarica la pagina!`;
 }else{
-  console.log("Ti tieni il prezzo intero");
+  
+  const euroKm= 0.21;
+  
+  let ticketCost= userKm * euroKm;
+  
+  console.log("Prezzo intero");
+  console.log(ticketCost);
+  
+  if (userAge < 18){
+    console.log("Prezzo per minorenni")
+    ticketCost= ticketCost * 80 / 100;
+  } else if (userAge > 65){
+    console.log("Prezzo per over 65")
+    ticketCost= ticketCost * 60 / 100;
+  }else{
+    console.log("Ti tieni il prezzo intero");
+  }
+  console.log(ticketCost);
+  
+  document.getElementById("output").innerHTML= `Il tuo biglietto costa ${ticketCost.toFixed(2)}&euro;`;
 }
-console.log(ticketCost);
-
-document.getElementById("output").innerHTML= `Il tuo biglietto costa ${ticketCost.toFixed(2)}&euro;`;
